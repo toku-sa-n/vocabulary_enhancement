98abda7e18bdd6d3b7a2a129040164ce47c5b65e
toku-sa-n <tokuchan3515@gmail.com>
2019-11-27 10:35:52 +0900

    [Add]Show the content of csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate csv;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rdr = csv::Reader::from_reader(std::io::stdin());
+
+    for result in rdr.records() {
+        let record = result.expect("a CSV record");
+
+        println!("{:?}", record);
+    }
 }
