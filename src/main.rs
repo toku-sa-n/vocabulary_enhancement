ea3732083c242d66b52881ea1353ba45068e0489
toku-sa-n <tokuchan3515@gmail.com>
2019-11-27 12:43:34 +0900

    [Add]Show an error when file is not specified

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,9 @@ fn main() {
 
 fn read_csv_from_arg() -> std::result::Result<csv::Reader<std::fs::File>, csv::Error> {
     let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Specify a file to read!");
+        std::process::exit(1);
+    }
     csv::Reader::from_path(&args[1])
 }
