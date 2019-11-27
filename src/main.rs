53f996e08285a433362946e7a9e6e25586b6fa97
toku-sa-n <tokuchan3515@gmail.com>
2019-11-27 12:36:27 +0900

    [Change]Input from first argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate csv;
 
 fn main() {
-    let mut rdr = csv::Reader::from_reader(std::io::stdin());
+    let mut rdr = read_csv_from_arg().unwrap();
 
     for result in rdr.records() {
         let record = result.expect("a CSV record");
 
         println!("{:?}", record);
     }
+}
+
+fn read_csv_from_arg() -> std::result::Result<csv::Reader<std::fs::File>, csv::Error> {
+    let args: Vec<String> = std::env::args().collect();
+    csv::Reader::from_path(&args[1])
 }
